224799be7a40eb5ce70bc22a5f5af1424e5cc4c2
林典群 <davidturtle0313@gmail.com>
2020-06-17 11:40:52 +0800

    remove remark from GOogle SignedUrlArrayCast

--- src/Casts/GoogleSignedUrlArrayCast.php
+++ src/Casts/GoogleSignedUrlArrayCast.php
@@ -6,15 +6,6 @@ use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class GoogleSignedUrlArrayCast implements CastsAttributes
 {
-  /**
-   * Cast the given value.
-   *
-   * @param  \Illuminate\Database\Eloquent\Model  $model
-   * @param  string  $key
-   * @param  mixed  $value
-   * @param  array  $attributes
-   * @return array
-   */
   public function get($model, $key, $value, $attributes)
   {
     if (!$value) {
@@ -35,15 +26,6 @@ class GoogleSignedUrlArrayCast implements CastsAttributes
     return $_value;
   }
 
-  /**
-   * Prepare the given value for storage.
-   *
-   * @param  \Illuminate\Database\Eloquent\Model  $model
-   * @param  string  $key
-   * @param  array  $value
-   * @param  array  $attributes
-   * @return string
-   */
   public function set($model, $key, $value, $attributes)
   {
     if (is_null($value)) {

89eb7f66bdd5f491cdf8f46d5959c9427ee6e6c7
林典群 <davidturtle0313@gmail.com>
2020-07-19 08:22:11 +0800

    remove other

--- src/Controllers/FileController.php
+++ src/Controllers/FileController.php
@@ -7,10 +7,10 @@ use Storage;
 
 class FileController extends Controller
 {
-  public function auth_file($auth, $auth_id, $type, $repo, $name)
+  public function file_idmatch($model, $model_id, $type, $repo, $name)
   {
     $disk         = Storage::disk('gcs');
-    $storage_path = "{$auth}/{$auth_id}/{$type}/{$repo}/{$name}";
+    $storage_path = "{$model}/{$model_id}/{$type}/{$repo}/{$name}";
     try {
       $mimeType = $disk->mimeType($storage_path);
     } catch (\Throwable $th) {
@@ -28,45 +28,45 @@ class FileController extends Controller
     return response($file)->header('Content-Type', $mimeType);
   }
 
-  public function show($type, $model, $repo, $name)
-  {
-    $disk         = Storage::disk('gcs');
-    $storage_path = "@{$type}/{$model}/{$repo}/{$name}";
-    try {
-      $mimeType = $disk->mimeType($storage_path);
-    } catch (\Throwable $th) {
-      return response()->json([
-        'message' => 'get file type fail or find no file.',
-      ], 400);
-    }
-    try {
-      $file = $disk->get($storage_path);
-    } catch (\Throwable $th) {
-      return response()->json([
-        'message' => 'get file type fail or find no file.',
-      ], 400);
-    }
-    return response($file)->header('Content-Type', $mimeType);
-  }
+  // public function show($type, $model, $repo, $name)
+  // {
+  //   $disk         = Storage::disk('gcs');
+  //   $storage_path = "@{$type}/{$model}/{$repo}/{$name}";
+  //   try {
+  //     $mimeType = $disk->mimeType($storage_path);
+  //   } catch (\Throwable $th) {
+  //     return response()->json([
+  //       'message' => 'get file type fail or find no file.',
+  //     ], 400);
+  //   }
+  //   try {
+  //     $file = $disk->get($storage_path);
+  //   } catch (\Throwable $th) {
+  //     return response()->json([
+  //       'message' => 'get file type fail or find no file.',
+  //     ], 400);
+  //   }
+  //   return response($file)->header('Content-Type', $mimeType);
+  // }
 
-  public function show_with_parent($parent, $parent_id, $type, $model, $repo, $name)
-  {
-    $disk         = Storage::disk('gcs');
-    $storage_path = "{$parent}/{$parent_id}/@{$type}/{$model}/{$repo}/{$name}";
-    try {
-      $mimeType = $disk->mimeType($storage_path);
-    } catch (\Throwable $th) {
-      return response()->json([
-        'message' => 'get file type fail or find no file.',
-      ], 400);
-    }
-    try {
-      $file = $disk->get($storage_path);
-    } catch (\Throwable $th) {
-      return response()->json([
-        'message' => 'get file type fail or find no file.',
-      ], 400);
-    }
-    return response($file)->header('Content-Type', $mimeType);
-  }
+  // public function show_with_parent($parent, $parent_id, $type, $model, $repo, $name)
+  // {
+  //   $disk         = Storage::disk('gcs');
+  //   $storage_path = "{$parent}/{$parent_id}/@{$type}/{$model}/{$repo}/{$name}";
+  //   try {
+  //     $mimeType = $disk->mimeType($storage_path);
+  //   } catch (\Throwable $th) {
+  //     return response()->json([
+  //       'message' => 'get file type fail or find no file.',
+  //     ], 400);
+  //   }
+  //   try {
+  //     $file = $disk->get($storage_path);
+  //   } catch (\Throwable $th) {
+  //     return response()->json([
+  //       'message' => 'get file type fail or find no file.',
+  //     ], 400);
+  //   }
+  //   return response($file)->header('Content-Type', $mimeType);
+  // }
 }

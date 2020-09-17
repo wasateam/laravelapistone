1390f56f7972a11016b8872bb49a065ecb9d1ea8
林典群 <davidturtle0313@gmail.com>
2020-09-17 10:03:24 +0800

    json cast

--- src/Casts/JsonCast.php
+++ src/Casts/JsonCast.php
@@ -30,6 +30,6 @@ class JsonCast implements CastsAttributes
    */
   public function set($model, $key, $value, $attributes)
   {
-    return json_encode($value);
+    return json_encode($value, JSON_UNESCAPED_UNICODE);
   }
 }

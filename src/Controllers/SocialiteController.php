46889f7498c3d16c2724e5894fe11d79034a3db8
林典群 <davidturtle0313@gmail.com>
2021-01-26 22:46:37 +0800

    remove useless

--- src/Controllers/SocialiteController.php
+++ src/Controllers/SocialiteController.php
@@ -6,7 +6,6 @@ namespace Wasateam\Laravelapistone\Controllers;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Socialite;
 use Wasateam\Laravelapistone\Services\SocialiteAccountService;
 
@@ -15,19 +14,5 @@ class SocialiteController extends Controller
   public function googleCallback(SocialiteAccountService $service, Request $request)
   {
     return $service->createOrGetUser(Socialite::driver('google')->stateless()->user(), 'google');
-  }
-
-  public function redirectLineToProvider()
-  {
-    return Socialite::with('line')->redirect();
-  }
-  /**
-   * Redirect the user to the GitHub authentication page.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function redirectToProvider($provider)
-  {
-    return Socialite::driver($provider)->redirect();
   }
 }

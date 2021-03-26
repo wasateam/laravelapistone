509f2e6d6bd3b254f1bf225a89c74b52de13e559
林典群 <davidturtle0313@gmail.com>
2021-03-26 10:54:08 +0800

    gcs helper svg content-type

--- src/Helpers/GcsHelper.php
+++ src/Helpers/GcsHelper.php
@@ -7,8 +7,17 @@ use Storage;
 
 class GcsHelper
 {
-  public static function getUploadSignedUrlByNameAndPath($file_name, $file_path, $contentType = 'image/*')
+  public static function getUploadSignedUrlByNameAndPath($file_name, $file_path, $contentType = 'image/svg+xml')
   {
+    $type = strtolower(explode('.', $file_name)[count(explode('.', $file_name)) - 1]);
+    if ($type == 'svg') {
+      $contentType = 'image/svg+xml';
+    } else if ($type == 'ico' || $type == 'jpg' || $type == 'jpeg' || $type == 'png') {
+      $contentType = 'image/*';
+    } else {
+      $contentType = '*';
+    }
+
     $disk        = Storage::disk('gcs');
     $random_path = self::getRandomPath();
     $path        = "{$file_path}/{$random_path}/{$file_name}";

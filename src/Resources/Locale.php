ae637f7002b75040803214f63510379d58e15b32
hsinyun <ruby8941@gmail.com>
2021-06-16 10:21:38 +0800

    [issue] locale resource change Admin -> Admin_R1

--- src/Resources/Locale.php
+++ src/Resources/Locale.php
@@ -17,8 +17,8 @@ class Locale extends JsonResource
     if (config('stone.mode') == 'cms') {
       return [
         'id'            => $this->id,
-        'updated_admin' => new Admin($this->updated_admin),
-        'created_admin' => new Admin($this->created_admin),
+        'updated_admin' => new Admin_R1($this->updated_admin),
+        'created_admin' => new Admin_R1($this->created_admin),
         'sequence'      => $this->sequence,
         'updated_at'    => $this->updated_at,
         'created_at'    => $this->created_at,

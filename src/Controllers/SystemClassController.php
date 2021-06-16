7a4393c25a2473b677cbdb0b767e491aaa9221ae
林典群 <davidturtle0313@gmail.com>
2021-06-16 16:43:11 +0800

    system_class index_with_area fix

--- src/Controllers/SystemClassController.php
+++ src/Controllers/SystemClassController.php
@@ -60,7 +60,7 @@ class SystemClassController extends Controller
    */
   public function index_with_area(Request $request, $area = null)
   {
-    return ModelHelper::ws_IndexHandler($this, $request, null, false, function ($snap) {
+    return ModelHelper::ws_IndexHandler($this, $request, null, false, function ($snap) use ($area) {
       $snap = $snap->where('area_id', $area);
       return $snap;
     });

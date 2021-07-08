71ed3ad39d91dd0938d30f9a03082ee01e0b7127
davidturtle <davidturtle0313@gmail.com>
2021-07-08 18:17:38 +0800

    stone

--- config/stone.php
+++ config/stone.php
@@ -1,21 +1,22 @@
 <?php
 
 return [
-  'mode'    => 'cms', // cms, webapi
+  'mode'    => env('STONE_MODE', 'cms'), // cms, webapi
   'storage' => [
-    'signed_url' => false,
-    'service'    => 'gcs', // gcs, app, s3
+    'signed_url' => env('STONE_STORAGE_SIGNED_URL', false),
+    'service'    => env('STONE_STORAGE_SERVICE', 'gcs'), // gcs, app, s3
     'gcs'        => [
       'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', null),
     ],
+    'acl'        => env('STONE_STORAGE_ACL', true),
   ],
   'mail'    => [
-    'service'    => 'surenotify', // gmail, surenotify
+    'service'    => env('gmail'), // gmail, surenotify
     'api_key'    => env('MAIL_API_KEY'),
     'api_domain' => env('MAIL_API_DOMAIN'),
   ],
   'log'     => [
-    'is_active' => true,
+    'is_active' => env('STONE_LOG_ACTIVE', true),
     'model'     => '\Wasateam\Laravelapistone\Models\CmsLog',
     // 'model'     => '\Wasateam\Laravelapistone\Models\WebLog',
   ],
@@ -23,15 +24,15 @@ return [
   'web_url' => env('WEB_URL'),
   // User
   'auth'    => [
-    'model_name'     => 'user',
-    'model'          => '\Wasateam\Laravelapistone\Models\User',
-    'resource'       => '\Wasateam\Laravelapistone\Resources\User',
-    'auth_scope'     => 'user',
-    'default_scopes' => [
+    'model_name'       => 'user',
+    'model'            => '\Wasateam\Laravelapistone\Models\User',
+    'resource'         => '\Wasateam\Laravelapistone\Resources\User',
+    'auth_scope'       => 'user',
+    'default_scopes'   => [
       'user',
     ],
-    'active_check'   => false,
-    'has_role'   => false,
+    'active_check'     => false,
+    'has_role'         => false,
     'has_system_class' => true,
   ],
   // Admin

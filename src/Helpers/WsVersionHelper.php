dba0811912342b75e97892d79695418b9fef3abf
hsinyun <ruby8941@gmail.com>
2021-07-21 09:25:11 +0800

    [issue] WsVersionHelper add document

--- src/Helpers/WsVersionHelper.php
+++ src/Helpers/WsVersionHelper.php
@@ -4,6 +4,9 @@ namespace Wasateam\Laravelapistone\Helpers;
 
 class WsVersionHelper
 {
+  //version_name=>所在version ex.audit_version
+  //model_name=>本身model_name ex.audit_question
+  //$model => 版本model ex.AuditQuestionVersion
   public static function getVersion($request, $version_name = null, $model_name = null, $resource = null, $order_by = null, $model = null)
   {
     $last_version = [];

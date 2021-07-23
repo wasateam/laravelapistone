a001d01349952f1c2c1c488b5cd837a59dfce71c
davidturtle <davidturtle0313@gmail.com>
2021-07-23 12:23:15 +0800

    tulpa page template R1

--- src/Resources/TulpaPageTemplate_R1.php
+++ src/Resources/TulpaPageTemplate_R1.php
@@ -4,7 +4,7 @@ namespace Wasateam\Laravelapistone\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TulpaPageTemplate extends JsonResource
+class TulpaPageTemplate_R1 extends JsonResource
 {
   /**
    * Transform the resource into an array.

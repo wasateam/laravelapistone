50ae34c3c74887c3f8b2140b02abbb19ec3afec0
davidturtle <davidturtle0313@gmail.com>
2021-09-02 14:22:18 +0800

    fix token

--- src/Helpers/RecaptchaHelper.php
+++ src/Helpers/RecaptchaHelper.php
@@ -6,7 +6,7 @@ class RecaptchaHelper
 {
   public static function verify($token)
   {
-    $secret_key = '6LfJoTocAAAAAK4YNur38dl_u6FmwjEs_9PaxU7v';
+    $secret_key = env('GOOGLE_RECAPTCHA_SECRETKEY');
     $recaptcha  = new ReCaptcha($secret_key);
     $resp       = $recaptcha
       ->setScoreThreshold(0.5)

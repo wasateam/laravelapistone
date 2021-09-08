461c53a2e16a1ea6523ecb9a7670efa8544ec9c2
davidturtle <davidturtle0313@gmail.com>
2021-09-08 14:43:25 +0800

    fox

--- src/Models/Appointment.php
+++ src/Models/Appointment.php
@@ -10,11 +10,6 @@ class Appointment extends Model
   use HasFactory;
   use \Illuminate\Database\Eloquent\SoftDeletes;
 
-  public function appointmemnt_available()
-  {
-    return $this->belongsTo(AppointmentAvailable::class, 'appointmemnt_available_id');
-  }
-
   public function user()
   {
     return $this->belongsTo(User::class, 'user_id');
@@ -22,6 +17,6 @@ class Appointment extends Model
 
   public function service_store()
   {
-    return $this->belongsTo(ServiceStore_R1::class, 'service_store_id');
+    return $this->belongsTo(ServiceStore::class, 'service_store_id');
   }
 }

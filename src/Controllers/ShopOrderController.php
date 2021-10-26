f5bc659a55f0df19dd7593cece4724a67d85e7d1
Billy <guanyiji0407@gmail.com>
2021-10-26 17:43:30 +0800

    shopOrder resouces、controller content

--- src/Controllers/ShopOrderController.php
+++ src/Controllers/ShopOrderController.php
@@ -4,30 +4,53 @@ namespace Wasateam\Laravelapistone\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use Wasateam\Laravelapistone\Helpers\ModelHelper;
 
 
 class ShopOrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public $model                   = 'Wasateam\Laravelapistone\Models\ShopOrder';
+    public $name                    = 'shop_order';
+    public $resource                = 'Wasateam\Laravelapistone\Resources\ShopOrder';
+    // public $resource_for_collection = 'Wasateam\Laravelapistone\Resources\ShopProductCollection';
+    public $input_fields            = [
+      'id',
+      'no',
+      'type',
+      'selected',
+      'order_time',
+      'receiver',
+      'receiver_tel',
+      'receiver_address',
+      'receive_remark',
+      'package_methods',
+      'order_status',
+      'logistics_methods',
+      'delivery_time',
+      'delivery_remark',
+      'shipment_status',
+      'shipment_date',
+      'customer_service',
+    ];
+    public $search_fields = [
+        // 'no',
+        // 'uuid',
+    ];
+    public $belongs_to = [
+    ];
+    public $filter_fields = [
+    ];
+    public $belongs_to_many = [
+    ];
+    public $filter_belongs_to_many = [
+    ];
+    public $order_fields = [
+    ];
+    public $uuid = false;
+
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return ModelHelper::ws_IndexHandler($this, $request, $id);
     }
 
     /**
@@ -36,9 +59,9 @@ class ShopOrderController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $id = null)
     {
-        //
+        return ModelHelper::ws_StoreHandler($this, $request, $id);
     }
 
     /**
@@ -47,20 +70,9 @@ class ShopOrderController extends Controller
      * @param  \App\Models\ShopOrder  $shopOrder
      * @return \Illuminate\Http\Response
      */
-    public function show(ShopOrder $shopOrder)
+    public function show(Request $request, $id = null)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\ShopOrder  $shopOrder
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(ShopOrder $shopOrder)
-    {
-        //
+      return ModelHelper::ws_ShowHandler($this, $request, $id);
     }
 
     /**
@@ -70,9 +82,9 @@ class ShopOrderController extends Controller
      * @param  \App\Models\ShopOrder  $shopOrder
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, ShopOrder $shopOrder)
+    public function update(Request $request, $id)
     {
-        //
+      return ModelHelper::ws_UpdateHandler($this, $request, $id);
     }
 
     /**
@@ -81,8 +93,32 @@ class ShopOrderController extends Controller
      * @param  \App\Models\ShopOrder  $shopOrder
      * @return \Illuminate\Http\Response
      */
-    public function destroy(ShopOrder $shopOrder)
+    public function destroy($id)
     {
-        //
+      return ModelHelper::ws_DestroyHandler($this, $id);
     }
+
+      /**
+   * Export Excel Signedurl
+   *
+   */
+  public function export_excel_signedurl(Request $request)
+  {
+    $shop_orders = $request->has('shop_orders') ? $request->shop_orders : null;
+    return URL::temporarySignedRoute(
+      'user_export_excel',
+      now()->addMinutes(30),
+      ['shop_orders' => $shop_orders]
+    );
+  }
+
+  /**
+   * Export Excel
+   *
+   */
+  public function export_excel(Request $request)
+  {
+    $shop_orders = $request->has('shop_orders') ? $request->shop_orders : null;
+    return Excel::download(new UserExport($shop_orders), 'user.xlsx');
+  }
 }
--- src/Resources/ShopOrder.php
+++ src/Resources/ShopOrder.php
@@ -14,31 +14,25 @@ class ShopOrder extends JsonResource
    */
   public function toArray($request)
   {
-    // if (config('stone.mode') == 'cms') {
-    //   $res = [
-    //     'id'              => $this->id,
-    //     'created_at'      => $this->created_at,
-    //     'updated_at'      => $this->updated_at,
-    //     'name'            => $this->name,
-    //     'sq'              => $this->sq,
-    //     'type'            => $this->type,
-    //     'shop_subclasses' => ShopSubclass_R1::collection($this->shop_subclasses),
-    //   ];
-    //   if (config('stone.shop.uuid')) {
-    //     $res['uuid'] = $this->uuid;
-    //   }
-    // } else if (config('stone.mode') == 'webapi') {
-    //   $res = [
-    //     'id'              => $this->id,
-    //     'name'            => $this->name,
-    //     'sq'              => $this->sq,
-    //     'type'            => $this->type,
-    //     'shop_subclasses' => ShopSubclass_R1::collection($this->shop_subclasses),
-    //   ];
-    //   if (config('stone.shop.uuid')) {
-    //     $res['uuid'] = $this->uuid;
-    //   }
-    // }
-    // return $res;
+    $res = [
+      'id'                       => $this->id,
+      'no'                       => $this->no,
+      'type'                     => $this->type,
+      'selected'                 => $this->selected,
+      'order_time'               => $this->order_time,
+      'receiver'                 => $this->receiver,
+      'receiver_tel'             => $this->receiver_tel,
+      'receiver_address'         => $this->receiver_address,
+      'receive_remark'           => $this->receive_remark,
+      'package_methods'          => $this->package_methods,
+      'order_status'             => $this->order_status,
+      'logistics_methods'        => $this->logistics_methods,
+      'delivery_time'            => $this->delivery_time,
+      'delivery_remark'          => $this->delivery_remark,
+      'shipment_status'          => $this->shipment_status,
+      'shipment_date'            => $this->shipment_date,
+      'customer_service'         => $this->customer_service,
+    ];
+    return $res;
   }
 }

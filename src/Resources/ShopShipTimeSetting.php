8ce645997524f32a6123a36604f34b2ce491e4f6
davidturtle <davidturtle0313@gmail.com>
2021-10-28 11:35:43 +0800

    time setting id

--- src/Resources/ShopShipTimeSetting.php
+++ src/Resources/ShopShipTimeSetting.php
@@ -16,12 +16,14 @@ class ShopShipTimeSetting extends JsonResource
   {
     if (config('stone.mode') == 'cms') {
       $res = [
+        'id'         => $this->id,
         'start_time' => $this->start_time,
         'end_time'   => $this->end_time,
         'max_count'  => $this->max_count,
       ];
     } else if (config('stone.mode') == 'webapi') {
       $res = [
+        'id'         => $this->id,
         'start_time' => $this->start_time,
         'end_time'   => $this->end_time,
         'max_count'  => $this->max_count,

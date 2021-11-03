a75685dabda9d76e1d840f5a84f767bb412c1f29
davidturtle <davidturtle0313@gmail.com>
2021-11-03 22:04:31 +0800

    fix

--- src/Controllers/WsBlogController.php
+++ src/Controllers/WsBlogController.php
@@ -37,10 +37,10 @@ class WsBlogController extends Controller
     'cover_image',
   ];
   public $belongs_to_many = [
-    'ws_blogs_classes',
+    'ws_blog_classes',
   ];
   public $filter_belongs_to_many = [
-    'ws_blogs_classes',
+    'ws_blog_classes',
   ];
   public $order_fields = [
     'publish_at',

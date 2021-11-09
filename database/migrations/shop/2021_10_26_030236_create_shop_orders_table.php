f6d6e70d7396c841c94af698436580f7e7a65dda
hsinyun <ruby8941@gmail.com>
2021-11-09 13:59:51 +0800

    [shoporder] shop order migration table

--- database/migrations/shop/2021_10_26_030236_create_shop_orders_table.php
+++ database/migrations/shop/2021_10_26_030236_create_shop_orders_table.php
@@ -6,64 +6,73 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateShopOrdersTable extends Migration
 {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
     /**
-     * Run the migrations.
+     * 目前尚未分類 cms、webapi 欄位
      *
-     * @return void
+     *
+     *
      */
-    public function up()
-    {
-        /**
-         * 目前尚未分類 cms、webapi 欄位
-         *
-         * 
-         * 
-         */
-        Schema::create('shop_orders', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->uuid('uuid')->nullable()->unique();
-            // 訂單類型 （種類）
-            $table->string('type')->nullable(); 
-            // 訂單編號
-            $table->string('no')->nullable(); 
-            // 收件者
-            $table->string('receiver')->nullable(); 
-            // 收件人電話
-            $table->string('receiver_tel')->nullable(); 
-            // 地址
-            $table->string('address')->nullable(); 
-            // 收件備註
-            $table->text('receive_remark')->nullable(); 
-            // 包裝方式
-            $table->string('package_methods')->nullable(); 
-            // 訂單狀態
-            $table->string('status')->nullable(); 
-            // 訂單備註狀態
-            $table->string('remark_status')->nullable(); 
-            // 物流方式
-            $table->string('deliver_way')->nullable(); 
-            // 配送時段
-            $table->time('delivery_time')->nullable(); 
-            // 配送備註
-            $table->text('delivery_remark')->nullable(); 
-            // 客服備註
-            $table->text('customer_service_remark')->nullable(); 
-            // Area
-            $table->string('area_id')->nullable();
-            // AreaSection
-            $table->string('area_section_id')->nullable();
-            // 付款狀態關連到 付款資訊
-        });
-    }
+    Schema::create('shop_orders', function (Blueprint $table) {
+      $table->id();
+      $table->timestamps();
+      $table->softDeletes();
+      $table->uuid('uuid')->nullable()->unique();
+      $table->integer('created_user_id')->nulllable();
+      $table->integer('updated_user_id')->nulllable();
+      // 訂單類型 （種類）
+      $table->string('type')->nullable();
+      // 訂單編號
+      $table->string('no')->nullable();
+      //訂購者
+      $table->text('orderer')->nullable();
+      $table->text('orderer_tel')->nullable();
+      $table->date('orderer_birthday')->nullable();
+      $table->text('orderer_email')->nullable();
+      $table->text('orderer_gender')->nullable();
+      // 收件者
+      $table->text('receiver')->nullable();
+      $table->text('receiver_tel')->nullable();
+      $table->text('receiver_email')->nullable();
+      $table->text('receiver_gender')->nullable();
+      $table->date('receiver_birthday')->nullable();
+      // 收件地址
+      $table->integer('user_address_id')->nullable();
+      // 收件備註
+      $table->text('receive_remark')->nullable();
+      // 包裝方式
+      $table->integer('package_method_id')->nullable();
+      // 訂單狀態
+      $table->integer('status')->nullable();
+      // 訂單備註狀態
+      $table->text('status_remark')->nullable();
+      // 物流方式
+      $table->integer('deliver_way_id')->nullable();
+      $table->time('delivery_time')->nullable();
+      $table->text('delivery_remark')->nullable();
+      // 客服備註
+      $table->text('customer_service_remark')->nullable();
+      // Area
+      $table->integer('area_id')->nullable();
+      // AreaSection
+      $table->integer('area_section_id')->nullable();
+      // 付款狀態關連到 付款資訊
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('shop_orders');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('shop_orders');
+  }
 }

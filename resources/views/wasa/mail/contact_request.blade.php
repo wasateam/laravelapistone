bf0198f555685091f5e306ce0ab9f00501c74f37
davidturtle <davidturtle0313@gmail.com>
2021-12-08 14:53:03 +0800

    margin

--- resources/views/wasa/mail/contact_request.blade.php
+++ resources/views/wasa/mail/contact_request.blade.php
@@ -1,29 +1,29 @@
 @if(isset($contact_request['name'])&&in_array('name',$fields))
-<h4>Name:</h4>
-<p>{{$contact_request['name']}}</p>
+<h4 style="margin:0">Name:</h4>
+<p style="margin:0">{{$contact_request['name']}}</p>
 <br>
 @endif
 
 @if(isset($contact_request['email'])&&in_array('email',$fields))
-<h4>Email:</h4>
-<p>{{$contact_request['email']}}</p>
+<h4 style="margin:0">Email:</h4>
+<p style="margin:0">{{$contact_request['email']}}</p>
 <br>
 @endif
 
 @if(isset($contact_request['tel'])&&in_array('tel',$fields))
-<h4>Tel:</h4>
-<p>{{$contact_request['tel']}}</p>
+<h4 style="margin:0">Tel:</h4>
+<p style="margin:0">{{$contact_request['tel']}}</p>
 <br>
 @endif
 
 @if(isset($contact_request['company_name'])&&in_array('company_name',$fields))
-<h4>Company:</h4>
-<p>{{$contact_request['company_name']}}</p>
+<h4 style="margin:0">Company:</h4>
+<p style="margin:0">{{$contact_request['company_name']}}</p>
 <br>
 @endif
 
 @if(isset($contact_request['budget'])&&in_array('budget',$fields))
-<h4>預算</h4>
+<h4 style="margin:0">預算</h4>
 @if($contact_request->budget=='nolimit')
 <p>老子有錢</p>
 @endif
@@ -40,13 +40,13 @@
 @endif
 
 @if(isset($contact_request['remark'])&&in_array('remark',$fields))
-<h4>Description:</h4>
-<p>{{$contact_request['remark']}}</p>
+<h4 style="margin:0">Description:</h4>
+<p style="margin:0">{{$contact_request['remark']}}</p>
 <br>
 @endif
 
 @if(isset($contact_request['ip'])&&in_array('ip',$fields))
-<h4>IP:</h4>
-<p>{{$contact_request['ip']}}</p>
+<h4 style="margin:0">IP:</h4>
+<p style="margin:0">{{$contact_request['ip']}}</p>
 <br>
 @endif

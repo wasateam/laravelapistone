2ef6006645948ea93a8b3e856d1cd8918e2a759b
davidturtle <davidturtle0313@gmail.com>
2022-01-12 12:18:42 +0800

    fix user device record

--- src/Helpers/UserDeviceHelper.php
+++ src/Helpers/UserDeviceHelper.php
@@ -8,10 +8,10 @@ class UserDeviceHelper
 {
   public static function user_device_record($action, $user_device, $user)
   {
-    $user_device_modify_record              = new UserDeviceModifyRecord;
-    $user_device_modify_record->action      = $action;
-    $user_device_modify_record->user_device = $user_device->id;
-    $user_device_modify_record->user        = $user->id;
+    $user_device_modify_record                 = new UserDeviceModifyRecord;
+    $user_device_modify_record->action         = $action;
+    $user_device_modify_record->user_device_id = $user_device->id;
+    $user_device_modify_record->user           = $user->id;
     $user_device_modify_record->save();
   }
 }

5da4a2ff889fc6f877290e461dc7c5bb565dfbe4
hsinyun <ruby8941@gmail.com>
2022-01-17 17:39:08 +0800

    [issue] ShopCamapign add discount_way

--- src/Resources/ShopCampaign_R1.php
+++ src/Resources/ShopCampaign_R1.php
@@ -14,19 +14,41 @@ class ShopCampaign_R1 extends JsonResource
    */
   public function toArray($request)
   {
-    return [
-      "id"               => $this->id,
-      "name"             => $this->name,
-      "type"             => $this->type,
-      "start_date"       => $this->start_date,
-      "end_date"         => $this->end_date,
-      "discount_code"    => $this->discount_code,
-      "condition"        => $this->condition,
-      "full_amount"      => $this->full_amount,
-      "discount_percent" => $this->discount_percent,
-      "discount_amount"  => $this->discount_amount,
-      "limit"            => $this->limit,
-      "is_active"        => $this->is_active,
-    ];
+    if (config('stone.mode') == 'cms') {
+      $res = [
+        'updated_at'       => $this->updated_at,
+        'created_at'       => $this->created_at,
+        "id"               => $this->id,
+        "name"             => $this->name,
+        "type"             => $this->type,
+        "start_date"       => $this->start_date,
+        "end_date"         => $this->end_date,
+        "discount_code"    => $this->discount_code,
+        "condition"        => $this->condition,
+        "full_amount"      => $this->full_amount,
+        "discount_percent" => $this->discount_percent,
+        "discount_amount"  => $this->discount_amount,
+        "limit"            => $this->limit,
+        "is_active"        => $this->is_active,
+        "discount_way"     => $this->discount_way,
+      ];
+    } else if (config('stone.mode') == 'webapi') {
+      $res = [
+        "id"               => $this->id,
+        "name"             => $this->name,
+        "type"             => $this->type,
+        "start_date"       => $this->start_date,
+        "end_date"         => $this->end_date,
+        "discount_code"    => $this->discount_code,
+        "condition"        => $this->condition,
+        "full_amount"      => $this->full_amount,
+        "discount_percent" => $this->discount_percent,
+        "discount_amount"  => $this->discount_amount,
+        "limit"            => $this->limit,
+        "is_active"        => $this->is_active,
+        "discount_way"     => $this->discount_way,
+      ];
+    }
+    return $res;
   }
 }

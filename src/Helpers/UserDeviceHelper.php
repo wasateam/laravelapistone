5d7a5b40d9acb5be9454c32528d048e1f857bfb3
davidturtle <davidturtle0313@gmail.com>
2022-02-17 16:18:57 +0800

    user device record fix

--- src/Helpers/UserDeviceHelper.php
+++ src/Helpers/UserDeviceHelper.php
@@ -9,11 +9,13 @@ class UserDeviceHelper
 {
   public static function user_device_record($action, $user_device, $user)
   {
-    $user_device_modify_record                 = new UserDeviceModifyRecord;
-    $user_device_modify_record->action         = $action;
-    $user_device_modify_record->user_device_id = $user_device->id;
-    $user_device_modify_record->user_id        = $user->id;
-    $user_device_modify_record->service_plan_id = UserServicePlan::where('user_id',$user->id)->latest()->first();
+    $service_plan = UserServicePlan::where('user_id', $user->id)->latest()->get();
+
+    $user_device_modify_record                  = new UserDeviceModifyRecord;
+    $user_device_modify_record->action          = $action;
+    $user_device_modify_record->user_device_id  = $user_device->id;
+    $user_device_modify_record->user_id         = $user->id;
+    $user_device_modify_record->service_plan_id = $service_plan ? $service_plan->id : null;
     $user_device_modify_record->save();
   }
 }

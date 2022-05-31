3a2323ec3af5b0237dc083de4b96d97ede71f3ce
davidturtle <davidturtle0313@gmail.com>
2022-05-31 23:12:16 +0800

    fix admin

--- src/Resources/AdminCollection.php
+++ src/Resources/AdminCollection.php
@@ -15,9 +15,24 @@ class AdminCollection extends JsonResource
   public function toArray($request)
   {
     $res = [
-      'id'   => $this->id,
-      'name' => $this->name,
+      'id'    => $this->id,
+      'name'  => $this->name,
+      'email' => $this->email,
+      'country_code'      => $this->country_code,
+      'scopes'            => $this->scopes,
     ];
+
+    if (config('stone.admin_role')) {
+      $res['roles'] = AdminRole_R1::collection($this->roles);
+    }
+
+    if (config('stone.admin_group')) {
+      if (config('stone.admin_blur')) {
+        $res['cmser_groups'] = AdminGroup_R1::collection($this->cmser_groups);
+      } else {
+        $res['admin_groups'] = AdminGroup_R1::collection($this->admin_groups);
+      }
+    }
 
     return $res;
   }
